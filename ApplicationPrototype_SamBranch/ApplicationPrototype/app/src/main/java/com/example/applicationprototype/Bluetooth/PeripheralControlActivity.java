2044e555d0df8a65f01bee342e0f34b7ca422287
SamDressler <SamDressler>
2021-02-22 21:09:19 -0600

    cleaned up some duplicate files

--- ApplicationPrototype_SamBranch/ApplicationPrototype/app/src/main/java/com/example/applicationprototype/Bluetooth/PeripheralControlActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.applicationprototype.Bluetooth;
-
-import android.app.Activity;
-
-public class PeripheralControlActivity extends Activity {
-    public static final String EXTRA_NAME = "name";
-    public static final String EXTRA_ID = "id";
-
-}
